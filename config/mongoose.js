0eb600b0ffbb2416f922147bd9e2aae800cd90ad
imdivyachoudhary <divyachoudhary.avnc@gmail.com>
2023-06-08 00:46:49 +0530

    mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1/polling`);
-  // await mongoose.connect(`mongodb+srv://imdivyachoudhary:divya123@cluster0.lg0g4rt.mongodb.net/polling?retryWrites=true&w=majority`);
+  // await mongoose.connect(`mongodb://127.0.0.1/polling`);
+  await mongoose.connect(
+    `mongodb+srv://imdivyachoudhary:divya123@cluster0.lg0g4rt.mongodb.net/polling?retryWrites=true&w=majority`
+  );
   console.log(`Connected to DB : polling`);
 }
 
